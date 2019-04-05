640e6f9e10ba11d2c532421de657cfdbf0a49c41
Chang Yea Moon <changyeamoon@gmail.com>
2019-04-04 18:28:19 -0700

    modal the modal of the modal

---
+++ src/client/components/CustomizeModal.jsx
@@ -0,0 +1,89 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import { BrowserRouter as Router, Link, Switch, Route, Redirect } from 'react-router-dom';
+
+const Modal = styled.div.attrs({ className: 'MyModal' })`
+  display: none;
+  position: fixed;
+  z-index: 2;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgb(0, 0, 0);
+  background-color: rgba(0, 0, 0, 0.4);
+`;
+
+const ModalContent = styled.div`
+  background-color: #fefefe;
+  margin: 15% auto; /* 15% from the top and centered */
+  padding: 20px;
+  border: 1px solid #888;
+  width: 80%; /* Could be more or less, depending on screen size */
+`;
+
+const CloseButton = styled.span`
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+
+  &:focus,
+  &:hover {
+    color: black;
+    text-decoration: none;
+    cursor: pointer;
+  }
+`;
+
+const ComboEditor = styled.div`
+  display: grid;
+  grid-template-columns: auto 100px;
+`;
+
+const Entree = () => {
+  return <div>entree stuff</div>;
+};
+
+const Side = () => {
+  return <div>Side stuff</div>;
+};
+
+const Drink = () => {
+  return <div>Drink stuff</div>;
+};
+
+const CustomizeModal = ({ display, setDisplay }) => {
+  return (
+    <Modal style={{ display }}>
+      <ModalContent>
+        <CloseButton onClick={() => setDisplay('none')}>&times;</CloseButton>
+        <Router>
+          <ComboEditor>
+            <div>
+              <Switch>
+                <Route path="/entree" component={Entree} />
+                <Route path="/side" component={Side} />
+                <Route path="/drink" component={Drink} />
+              </Switch>
+            </div>
+            <ul>
+              <li>
+                <Link to="/entree">Entrée</Link>
+              </li>
+              <li>
+                <Link to="/side">Side</Link>
+              </li>
+              <li>
+                <Link to="/drink">Drink</Link>
+              </li>
+            </ul>
+          </ComboEditor>
+        </Router>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+export default CustomizeModal;
--- src/client/components/Item.jsx
+++ src/client/components/Item.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import CustomizeModal from './CustomizeModal';
 
 const ItemDetail = styled.div`
   display: grid;
@@ -10,16 +11,23 @@ const ItemPhoto = styled.span``;
 const ItemPrice = styled.span``;
 
 const Item = ({ addToCart, id, name, price, photo }) => {
+  const [display, setDisplay] = useState('none');
+
   return (
-    <ItemDetail>
-      <ItemName>{name} </ItemName>
-      <ItemPrice>${price} </ItemPrice>
-      <ItemPhoto>Photo Button{photo}</ItemPhoto>
-      <button type="button">Customize Then Add</button>
-      <button type="button" onClick={() => addToCart({ id, name, price })}>
-        Add to Cart
-      </button>
-    </ItemDetail>
+    <>
+      <ItemDetail>
+        <ItemName>{name} </ItemName>
+        <ItemPrice>${price} </ItemPrice>
+        <ItemPhoto>Photo Button{photo}</ItemPhoto>
+        <button type="button" onClick={() => setDisplay('block')}>
+          Customize Then Add
+        </button>
+        <button type="button" onClick={() => addToCart({ id, name, price })}>
+          Add to Cart
+        </button>
+      </ItemDetail>
+      <CustomizeModal display={display} setDisplay={setDisplay} />
+    </>
   );
 };
 
