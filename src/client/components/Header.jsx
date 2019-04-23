bcb886a270a0530d2e879f9dde16118fea495b2b
Chang Yea Moon <changyeamoon@gmail.com>
2019-04-22 21:35:10 -0700

    correct address

--- src/client/components/Header.jsx
+++ src/client/components/Header.jsx
@@ -119,10 +119,10 @@ const Header = () => {
           }}
         >
           <span style={{ padding: '0em 0.2em ' }} />
-          33862 Flat Shoals Rd,
+          3862 Flat Shoals Rd,
           <br />
           <span style={{ padding: '0em 0.15em ' }} />
-          Union City, GA 300291
+          Union City, GA 30291
         </P>
         <A
           href="https://www.google.com/maps/dir//3862+Flat+Shoals+Rd,+Union+City,+GA+30291/@33.5859943,-84.5188253,17z/data=!4m16!1m7!3m6!1s0x88f4e3f8a986ff4b:0xc41f278e32509327!2s3862+Flat+Shoals+Rd,+Union+City,+GA+30291!3b1!8m2!3d33.5859943!4d-84.5166366!4m7!1m0!1m5!1m1!1s0x88f4e3f8a986ff4b:0xc41f278e32509327!2m2!1d-84.5166366!2d33.5859943"
